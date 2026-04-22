ead6579024a96e3d53b3c61b663a24eeff41ed74
Daniel Marques <danielmateus631@gmail.com>
2026-04-22 21:01:48 +0100

    ft_split erro norminette

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: danicamp <danicamp@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 18:39:39 by danicamp          #+#    #+#             */
-/*   Updated: 2026/04/20 19:15:27 by danicamp         ###   ########.fr       */
+/*   Updated: 2026/04/22 20:59:03 by danicamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ static int	count_words(const char *s, char sep)
 
 	words = 0;
 	i = 0;
-	if (s[i] != sep && s[i + 1] != sep)
-		words++;
-	i++;
 	while (s[i])
 	{
-		if (s[i] == sep && s[i + 1] != sep)
+		while (s[i] == sep)
+			i++;
+		if (s[i])
 			words++;
-		i++;
+		while (s[i] && s[i] != sep)
+			i++;
 	}
 	return (words);
 }
@@ -46,31 +46,48 @@ static char	*extract_word(const char *s, int start, int len)
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**put_word(const char *s, char sep, char **words)
 {
-	char	**words;
-	int		i;
-	int		start;
-	int		j;
+	int	i;
+	int	j;
+	int	start;
 
 	i = 0;
 	j = 0;
 	start = 0;
-	words = malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!words || !s)
-		return (NULL);
 	while (s[i])
 	{
-		if (s[i] != c)
+		if (s[i] != sep)
 		{
 			start = i;
-			while (s[i] != c && s[i])
+			while (s[i] != sep && s[i])
 				i++;
 			words[j] = extract_word(s, start, i - start);
+			if (!words[j])
+			{
+				while (j > 0)
+					free(words[--j]);
+				free(words);
+				return (NULL);
+			}
 			j++;
 		}
-		i++;
+		else
+			i++;
 	}
-	words[count_words(s, c)] = NULL;
+	words[count_words(s, sep)] = NULL;
+	return (words);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**words;
+
+	if (!s)
+		return (NULL);
+	words = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!words)
+		return (NULL);
+	words = put_word(s, c, words);
 	return (words);
 }
